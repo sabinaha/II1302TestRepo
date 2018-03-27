87b754e8e70d1b924c782b191fb1ca7f14ea0560
Simon Östergren <simon.ostergren@gmail.com>
2018-03-27 09:41:02 +0200

    Fixat alla c-filer /Mrsred

---
+++ Source-Code/Include-common/ranta.h
@@ -0,0 +1,9 @@
+/* ranta.h */
+/* Developer MrsRead */ 
+
+#ifndef ranta_h
+  #define ranta_h
+  #define RANTESATS 8.5
+  
+  extern const      double          ranteFaktor;
+#endif
---
+++ Source-Code/Module-Main/Include-main/main.h
@@ -0,0 +1,12 @@
+/* This file is: main.h */ /* Developer MrsRed */ 
+#include "ranta.h" 
+#include "inmatnin.h" 
+#include "tabell.h" 
+#include "kalkylat.h" 
+#include "ranta.h" 
+#include <stdio.h> 
+
+#if !defined(main_h) 
+  #define main_hconst 
+  double ranteFaktor = RANTESATS / 100; /* RANTESATS hårdkodad i ranta.h */
+#endif
