e8af4afdf5651105545709334afccd151a52bbe4
Simon Östergren <simon.ostergren@gmail.com>
2018-03-27 10:42:35 +0200

    Fixade några saker /MissTurkos

--- Source-Code/Module-Inmatning/inmatning.c
+++ Source-Code/Module-Inmatning/inmatning.c
@@ -4,11 +4,11 @@
 
 void Inmatning( double* kapitalPek, int* antalArPek ){
 	/* clrscr();   /* finns ej i ANSI-C */  
-	printf("\nBer„knar kapitaltillv„xt vid %0.1f%% r„nta",RANTESATS);
-	printf("\n=============================================");
-	printf("\nPositivt kapital r„knar fram†t i tiden.");
-	printf("\nNegativt kapital r„knar bak†t i tiden.");
-	printf("\n\nInsatt kapital och antal †r ?(-->(+/-)1000 10)--> ");
+	printf("\nBeräknar kapitaltillv„xt vid %0.1f%% r„nta",RANTESATS);
+	printf("\n+=============================================+");
+	printf("\nPositivt kapital räknar framåt i tiden.");
+	printf("\nNegativt kapital räknar bakåt i tiden.");
+	printf("\n\nInsatt kapital och antal år ?(-->(+/-)1000 10)--> ");
 	scanf("%lf%d", kapitalPek, antalArPek);
 
 	return;
